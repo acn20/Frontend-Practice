07f61d9c38da446a45386739db00c2a64ef78da2
Andreea <andreea.nisipeanu@gmail.com>
2019-05-04 00:34:37 +0300

    extra elem solved

--- PRACTICE/extra element/extraElem.js
+++ PRACTICE/extra element/extraElem.js
@@ -1,21 +1,26 @@
-/*functie cu 2 parametri de tip arraBoy si functia returneaza elem care e in plus intre cele doua array-uri (voi primi array-uri care au tot in comun dar unul din array-uri are un elem extra)*/
+/*functie cu 2 parametri de tip array (doar cu numere) si functia returneaza elem care e in plus intre cele doua array-uri (voi primi array-uri care au tot in comun dar unul din array-uri are un elem extra)*/
 function extraElem(list1, list2) {
     if (Math.abs(list1.length - list2.length) != 1) {
         throw new Error("Please enter two lists that have the same elements except one of the list has an extra element");
     }
-    if (list1.legth > list2.length) {
-        for (let i = 0; i < list1.length; i++) {
-            if (list1[i] != list2[i]) {
-                return list1.splice(i, 1);
-            }
-        }
 
-    }
-    else {
+    list1.sort();//The sort() method sorts the elements of an array in place and returns the array.
+    list2.sort();
+
+    if (list1.length > list2.length) {
         for (let i = 0; i < list2.length; i++) {
             if (list1[i] != list2[i]) {
-                return list2.splice(i, 1);
+                return list1[i];
             }
         }
+        return list1[list1.length - 1];
+    }
+    else {
+        for (let i = 0; i < list1.length; i++) {
+            if (list1[i] != list2[i]) {
+                return list2[i];
+            }
+        }
+        return list2[list2.length - 1];
     }
 }
--- PRACTICE/extra element/test/extraElemSpec.js
+++ PRACTICE/extra element/test/extraElemSpec.js
@@ -1,33 +1,109 @@
 describe("extraElem tests", function () {
-    describe("When difference between lists lengths is not 1", function () {
+    describe("When the two arrays have the same length", function () {
         it("Throws error", function () {
-            var list1 = [1, 4, "1", 0, false];
-            var list2 = [1, 4, "1", 0, false];
+            var list1 = [1, 1, 0, 4];
+            var list2 = [1, 4, 1, 0];
 
             expect(function() { extraElem(list1, list2) }).toThrow();;
         })
     })
 
-    describe("When the first array has an extra element", function () {
-        it("Returns the extra element", function () {
-            var list1 = [1, 4, "1", 0, false];
-            var list2 = [1, "1", 0, false];
-            var result = extraElem(list1, list2);
+    describe("When the difference between lists lengths is greater than 1", function () {
+        it("Throws error", function () {
+            var list1 = [-2, 1, 4, 0, -1, 5, 10];
+            var list2 = [1, 4, -2, -1, 0];
 
-            expect(result).toEqual([4]);
+            expect(function() { extraElem(list1, list2) }).toThrow();;
         })
     })
 
-    describe("When the second array has an extra element", function () {
-        it("Returns the extra element", function () {
-            var list1 = [1, 4, "1", 0, false];
-            var list2 = [1, 4, "1", 0, false, "hello"];
-            var result = extraElem(list1, list2);
+    describe("When both arrays are empty", function () {
+        it("Throws error", function () {
+            var list1 = [];
+            var list2 = [];
 
-            expect(result).toEqual(["hello"]);
+            expect(function() { extraElem(list1, list2) }).toThrow();;
         })
     })
 
-    //daca primul array e mai lung, daca unul are 0 elem si altul 1 elem etc
+    describe("When the first array has zero elements and the second array has one element", function () {
+        it("Returns the extra element", function () {
+            var list1 = [];
+            var list2 = [5];
+            var result = extraElem(list1, list2);
+
+            expect(result).toBe(5);
+        })
+    })
+
+    describe("When the first array has one element and the second array has zero elements", function () {
+        it("Returns the extra element", function () {
+            var list1 = [5];
+            var list2 = [];
+            var result = extraElem(list1, list2);
+
+            expect(result).toBe(5);
+        })
+    })
+
+    describe("When the first array has an extra element at the beginning", function () {
+        it("Returns the extra element", function () {
+            var list1 = [4, 0, 1,-1, -3];
+            var list2 = [1, -3, 0, -1];
+            var result = extraElem(list1, list2);
+
+            expect(result).toBe(4);
+        })
+    })
+
+    describe("When the first array has an extra element in the middle", function () {
+        it("Returns the extra element", function () {
+            var list1 = [1, 4, 1, 0, -1];
+            var list2 = [1, 0, -1, 1];
+            var result = extraElem(list1, list2);
+
+            expect(result).toBe(4);
+        })
+    })
+
+    describe("When the first array has an extra element at the end", function () {
+        it("Returns the extra element", function () {
+            var list1 = [1, 11, 0, -1, 4];
+            var list2 = [0, 1, 11, -1];
+            var result = extraElem(list1, list2);
+
+            expect(result).toBe(4);
+        })
+    })
+
+    describe("When the second array has an extra element at the beginning", function () {
+        it("Returns the extra element", function () {
+            var list1 = [1, 0, 4, -1, 14];
+            var list2 = [8, 1, 4, 14, 0, -1];
+            var result = extraElem(list1, list2);
+
+            expect(result).toBe(8);
+        })
+    })
+
+    describe("When the second array has an extra element in the middle", function () {
+        it("Returns the extra element", function () {
+            var list1 = [1, 0, 13, 4, -1];
+            var list2 = [1, 4, 13, 8, 0, -1];
+            var result = extraElem(list1, list2);
+
+            expect(result).toBe(8);
+        })
+    })
+
+    describe("When the second array has an extra element at the end", function () {
+        it("Returns the extra element", function () {
+            var list1 = [11, 4, 1, 0, -1];
+            var list2 = [1, 4, 11, 0, -1, 9];
+            var result = extraElem(list1, list2);
+
+            expect(result).toBe(9);
+        })
+    })
 
 })
