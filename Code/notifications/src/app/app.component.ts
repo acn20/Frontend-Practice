f39137a4bbeae2a1a5816a9fe9206becdd41e5b9
Andreea <andreea.nisipeanu@gmail.com>
2020-01-22 01:42:07 +0200

    notifications

--- Code/notifications/src/app/app.component.ts
+++ Code/notifications/src/app/app.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'notifications';
+  show = false;
+  countNotifications: number = 0;
   
+  showNotifications() {
+    this.show = true;
+  }
 }
