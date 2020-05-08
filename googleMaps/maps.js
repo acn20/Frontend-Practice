7ac05c49498e72fc6b16e982b816ee5a85e77cb2
Andreea <andreea.nisipeanu@gmail.com>
2020-05-09 02:04:56 +0300

    airplanes

--- googleMaps/maps.js
+++ googleMaps/maps.js
@@ -1,6 +1,58 @@
+var map;
+
+function initMap() {
+
+    var airplanePositions = [{ lat: -27.467, lng: 153.027 }, { lat: -77.467, lng: 153.027 }, { lat: -27.467, lng: 103.027 }, { lat: -17.467, lng: 153.027 }, { lat: -87.467, lng: 173.027 }, { lat: -7.467, lng: 3.027 }, { lat: -15.467, lng: 53.027 }, { lat: -27.467, lng: 153.027 }, { lat: -7.467, lng: 113.027 }, { lat: 7.467, lng: 159.027 }];
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -27.467, lng: 153.027 },
+        zoom: 4,
+        mapTypeId: 'hybrid'
+    });
+
+    var image = 'images/airplaneIcon.png';
+
+    const airplaneMarkers = [];
+    for (let i = 0; i < airplanePositions.length; i++) {
+        airplaneMarkers.push(new google.maps.Marker({
+            position: airplanePositions[i],
+            map: map,
+            icon: image
+        }));
+    }
+
+    var flightPlanCoordinates = [
+        { lat: -27.467, lng: 153.027 },
+        { lat: -18.142, lng: 178.431 },
+        { lat: 21.291, lng: -157.821 },
+        { lat: 37.772, lng: -122.214 }
+    ];
+
+    var flightPath = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 1
+    });
+
+    flightPath.setMap(map);
+
+    setInterval(function () {
+        //airplaneMarker.setMap(null);
+        for (let i = 0; i < airplaneMarkers.length; i++) {
+            airplanePositions[i] = getCoordinates(airplanePositions[i].lat, airplanePositions[i].lng, 0.25, 0.75);
+            airplaneMarkers[i].setPosition(airplanePositions[i]);
+        }
+        //airplaneMarker.setMap(map);
+    }, 100);
+
+}
+
+
 function getLat(lat, speed) {
     let sum = lat + speed;
-    if ((sum) <= 90 || (sum) >= -90) {
+    if (sum >= -90 && sum <= 90) {
         return sum;
     }
     else {
@@ -15,63 +67,67 @@ function getLat(lat, speed) {
 
 function getLng(lng, speed) {
     let sum = lng + speed;
-    if ((sum) <= 180 || (sum) >= -180) {
+    if (sum >= -180 && sum <= 180) {
         return sum;
     }
     else {
         if (lng > 0 && speed > 0) {
             return -((180 - speed) + (180 - lng));
         }
-        if (lng < 0 && speed < 0) { return (180 + speed) + (-lng - 180); }
+        if (lng < 0 && speed < 0) {
+            return (180 + speed) + (180 + lng);
+        }
     }
-
 }
 
 function getCoordinates(lat, lng, latSpeed, lngSpeed) {
+
+    if (lat < -90 || lat > 90 || lng < -180 || lng > 180) {
+        throw new Error("The latitude must be between -90 or 90 inclusive and the longitude must be between -180 and 180 inclusive");
+    }
+
+    if (latSpeed <= -180 || latSpeed >= 180 || lngSpeed <= -360 || lngSpeed >= 360) {
+        throw new Error("Please do not exceed (-180, 180) interval for latitude speed and (-360, 360) interval for longitude speed");
+    }
+
     var newLat = lat;
     var newLng = lng;
 
     let sum1 = lat + latSpeed;
-    if ((sum1) <= 90 || (sum1) >= -90) {
-        newLat = sum1;
-    }
+    let sum2 = lng + lngSpeed;
 
+    if (sum1 >= -90 && sum1 <= 90) {
+        newLat = sum1;
+        if (sum2 >= -180 && sum2 <= 180) {
+            newLng = sum2;
+        }
+        else {
+            if (lng > 0 && lngSpeed > 0) {
+                newLng = -((180 - lngSpeed) + (180 - lng));
+            }
+            if (lng < 0 && lngSpeed < 0) {
+                newLng = (180 + lngSpeed) + (180 + lng);
+            }
+        };
+    }
     else {
         if (lat > 0 && latSpeed > 0) {
             newLat = (90 - latSpeed) + (90 - lat);
-            if (lng > 0) {
-                newLng = lng - 180;
-            }
-            else {
-                newLng = lng + 180;
-            }
 
-        }
+        };
         if (lat < 0 && latSpeed < 0) {
             newLat = (-90 - latSpeed) + (-90 - lat);
-            if (lng > 0) {
-                newLng = lng - 180;
-            }
-            else {
-                newLng = lng + 180;
-            }
+        };
+        if (lng > 0) {
+            newLng = lng - 180;
         }
+        else {
+            newLng = lng + 180;
+        };
     }
 
-    let sum2 = lng + lngSpeed;
-    if ((sum2) <= 180 || (sum2) >= -180) {
-        newLng = sum2;
-    }
-
-    else {
-        if (lng > 0 && lngSpeed > 0) {
-            newLng = -((180 - lngSpeed) + (180 - lng));
-        }
-        if (lng < 0 && lngSpeed < 0) {
-            newLng = (180 + lngSpeed) + (-lng - 180);
-        }
-    }
-
-    return ([newLat, newLng]);
-
+    return { lat: newLat, lng: newLng }
 }
+
+//if (latSpeed >= 180) { latSpeed = latSpeed%180 };
+//if (lngSpeed >= 360) { lngSpeed = lngSpeed%360 };
--- googleMaps/mapsSpec.js
+++ googleMaps/mapsSpec.js
@@ -1,42 +1,42 @@
 describe("getLat tests", function () {
 
-    describe("When lat + speed is between or equal -90 and 90", function () {
+    describe("When lat + speed is between -90 and 90 inclusive", function () {
         describe("When lat is positive and speed is positive", function () {
             it("Returns positive", function () {
                 var lat = 13;
                 var speed = 3;
                 var result = getLat(lat, speed);
-    
+
                 expect(result).toBe(16);
             })
         });
-    
+
         describe("When lat is positive and speed is negative", function () {
             it("Returns either positive or negative", function () {
                 var lat = 13;
                 var speed = -3;
                 var result = getLat(lat, speed);
-    
+
                 expect(result).toBe(10);
             })
         });
-    
+
         describe("When lat is negative and speed is negative", function () {
             it("Returns negative", function () {
                 var lat = -13;
                 var speed = -3;
                 var result = getLat(lat, speed);
-    
+
                 expect(result).toBe(-16);
             })
         });
-    
+
         describe("When lat is negative and speed is positive", function () {
             it("Returns either positive or negative", function () {
                 var lat = -13;
                 var speed = 3;
                 var result = getLat(lat, speed);
-    
+
                 expect(result).toBe(-10);
             })
         });
@@ -65,43 +65,43 @@ describe("getLat tests", function () {
 
 describe("getLng tests", function () {
 
-    describe("When lat + speed is between or equal -180 and 180", function () {
+    describe("When lat + speed is between -180 and 180 inclusive", function () {
         describe("When lng is positive and speed is positive", function () {
             it("Returns positive", function () {
                 var lng = 13;
                 var speed = 3;
                 var result = getLng(lng, speed);
-    
+
                 expect(result).toBe(16);
             })
         });
-    
+
         describe("When lng is positive and speed is negative", function () {
             it("Returns either positive or negative", function () {
                 var lng = 13;
                 var speed = -3;
                 var result = getLng(lng, speed);
-    
+
                 expect(result).toBe(10);
             })
         });
-    
+
         describe("When lng is negative and speed is negative", function () {
             it("Returns negative", function () {
                 var lng = -13;
                 var speed = -3;
                 var result = getLng(lng, speed);
-    
+
                 expect(result).toBe(-16);
             })
         });
-    
+
         describe("When lng is negative and speed is positive", function () {
             it("Returns either positive or negative", function () {
                 var lng = -13;
                 var speed = 3;
                 var result = getLng(lng, speed);
-    
+
                 expect(result).toBe(-10);
             })
         });
@@ -124,6 +124,174 @@ describe("getLng tests", function () {
             var result = getLng(lng, speed);
 
             expect(result).toBe(177);
+        })
+    });
+});
+
+describe("getCoordinates tests", function () {
+    describe("When lat + latSpeed is between -90 and 90 inclusive and lng + lngSpeed is between -180 and 180 inclusive", function () {
+        describe("When lat is positive, lng is positive, latSpeed is positive, lngSpeed is positive", function () {
+            it("Returns", function () {
+                var lat = 10;
+                var lng = 25;
+                var latSpeed = 5;
+                var lngSpeed = 10;
+                var result = getCoordinates(lat, lng, latSpeed, lngSpeed);
+
+                expect(result).toEqual({ lat: 15, lng: 35 });
+            })
+        });
+
+        describe("When lat is positive, lng is positive, latSpeed is negative, lngSpeed is negative", function () {
+            it("Returns", function () {
+                var lat = 10;
+                var lng = 25;
+                var latSpeed = -5;
+                var lngSpeed = -10;
+                var result = getCoordinates(lat, lng, latSpeed, lngSpeed);
+
+                expect(result).toEqual({ lat: 5, lng: 15 });
+            })
+        });
+    });
+
+    describe("When lat + latSpeed is greater than 90 and lng + lngSpeed is between -180 and 180 inclusive", function () {
+        it("Returns", function () {
+            var lat = 89;
+            var lng = 25;
+            var latSpeed = 5;
+            var lngSpeed = 0;
+            var result = getCoordinates(lat, lng, latSpeed, lngSpeed);
+
+            expect(result).toEqual({ lat: 86, lng: -155 });
+        });
+    });
+
+    describe("When lat + latSpeed is less than -90 and lng + lngSpeed is between -180 and 180 inclusive", function () {
+        it("Returns", function () {
+            var lat = -87;
+            var lng = 25;
+            var latSpeed = -5;
+            var lngSpeed = 0;
+            var result = getCoordinates(lat, lng, latSpeed, lngSpeed);
+
+            expect(result).toEqual({ lat: -88, lng: -155 });
+        })
+    });
+
+    describe("When lat + latSpeed is between -90 and 90 inclusive and lng + lngSpeed is greater than 180", function () {
+        it("Returns", function () {
+            var lat = 10;
+            var lng = 175;
+            var latSpeed = -5;
+            var lngSpeed = 10;
+            var result = getCoordinates(lat, lng, latSpeed, lngSpeed);
+
+            expect(result).toEqual({ lat: 5, lng: -175 });
+        })
+    });
+
+    describe("When lat + latSpeed is between -90 and 90 inclusive and lng + lngSpeed is less than -180", function () {
+        it("Returns", function () {
+            var lat = -10;
+            var lng = -177;
+            var latSpeed = 5;
+            var lngSpeed = -10;
+            var result = getCoordinates(lat, lng, latSpeed, lngSpeed);
+
+            expect(result).toEqual({ lat: -5, lng: 173 });
+        })
+    });
+
+    describe("When lat + latSpeed is less than -90 and lng + lngSpeed is less than -180", function () {
+        it("Returns", function () {
+            var lat = -88;
+            var lng = -175;
+            var latSpeed = -5;
+            var lngSpeed = 0;
+            var result = getCoordinates(lat, lng, latSpeed, lngSpeed);
+
+            expect(result).toEqual({ lat: -87, lng: 5 });
+        })
+    });
+
+    describe("When lat + latSpeed is greater than 90 and lng + lngSpeed is greater than 180", function () {
+        it("Returns", function () {
+            var lat = 86;
+            var lng = 177;
+            var latSpeed = 5;
+            var lngSpeed = 0;
+            var result = getCoordinates(lat, lng, latSpeed, lngSpeed);
+
+            expect(result).toEqual({ lat: 89, lng: -3 });
+        })
+    });
+
+    describe("When lat + latSpeed is less than -90 and lng + lngSpeed is greater than 180", function () {
+        it("Returns", function () {
+            var lat = -89;
+            var lng = 175;
+            var latSpeed = -5;
+            var lngSpeed = 0;
+            var result = getCoordinates(lat, lng, latSpeed, lngSpeed);
+
+            expect(result).toEqual({ lat: -86, lng: -5 });
+        })
+    });
+
+    describe("When lat + latSpeed is greater than 90 and lng + lngSpeed is less than -180", function () {
+        it("Returns", function () {
+            var lat = 89;
+            var lng = -179;
+            var latSpeed = 5;
+            var lngSpeed = 0;
+            var result = getCoordinates(lat, lng, latSpeed, lngSpeed);
+
+            expect(result).toEqual({ lat: 86, lng: 1 });
+        })
+    });
+
+    describe("When lat is less than -90 or greater than 90", function () {
+        it("Throws error", function () {
+            var lat = -93;
+            var lng = -179;
+            var latSpeed = 5;
+            var lngSpeed = 0;
+
+            expect(function() {getCoordinates(lat, lng, latSpeed, lngSpeed)}).toThrow();
+        })
+    });
+
+    describe("When lng is less than -180 or greater than 180", function () {
+        it("Throws error", function () {
+            var lat = -83;
+            var lng = 199;
+            var latSpeed = 5;
+            var lngSpeed = 10;
+
+            expect(function() {getCoordinates(lat, lng, latSpeed, lngSpeed)}).toThrow();
+        })
+    });
+
+    describe("When latSpeed is less or equal than -180 or greater or equal than 180", function () {
+        it("Throws error", function () {
+            var lat = -90;
+            var lng = -179;
+            var latSpeed = 185;
+            var lngSpeed = 0;
+
+            expect(function() {getCoordinates(lat, lng, latSpeed, lngSpeed)}).toThrow();
+        })
+    });
+
+    describe("When lngSpeed is less or equal than -360 or greater or equal than than 360", function () {
+        it("Throws error", function () {
+            var lat = -83;
+            var lng = 180;
+            var latSpeed = 5;
+            var lngSpeed = -410;
+
+            expect(function() {getCoordinates(lat, lng, latSpeed, lngSpeed)}).toThrow();
         })
     });
 })
